bfc0da4cb3bbee1a552a286c49e05abe079a30f7
Tatiana <tan.isa@mail.ru>
2020-08-23 09:04:28 +0300

    Add web integration

--- browser_action.js
+++ browser_action.js
@@ -1,2 +1,14 @@
 (function () {
+
+    el.addEventListener("click", function(){
+
+    text = document.getElementById("query-field").value;
+    const req = new XMLHttpRequest();
+    const baseUrl = "http://arina-8c2f80b7.localhost.run/chrome_extension";
+
+    req.open("POST", baseUrl, true);
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    req.send(text);
+
+    alert(text); });
 })();
